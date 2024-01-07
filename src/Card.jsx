d48511080ebeb408866e330509f0240bb2efd979
[satuhalinen] <satuh9@gmail.com>
2024-01-07 20:52:22 +0200

    add the clicking card changes the visibility of header and picture

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,12 @@
-const Card = ({ title, img }) => {
+const Card = ({ title, img, visibilityHandler, showDescription }) => {
   return (
-    <div className="card">
-      <h2>{title}</h2>
-      <img src={img}></img>
+    <div className="card" onClick={visibilityHandler}>
+      {showDescription === true ? (
+        <>
+          <h2>{title}</h2>
+          <img src={img}></img>
+        </>
+      ) : null}
     </div>
   );
 };
--- src/CardList.jsx
+++ src/CardList.jsx
@@ -1,13 +1,27 @@
 import Card from "./Card";
 import { cards } from "./cards";
-
+import { useState } from "react";
 const CardList = () => {
+  const [activeCardId, setActiveCardId] = useState(null);
+  const visibilityHandler = (id) => {
+    if (activeCardId === id) {
+      setActiveCardId(null);
+    } else {
+      setActiveCardId(id);
+    }
+  };
   return (
     <div className="card-grid">
       {cards.map(function (data) {
         return (
           <div>
-            <Card key={data.id} title={data.title} img={data.img}></Card>
+            <Card
+              visibilityHandler={() => visibilityHandler(data.id)}
+              showDescription={activeCardId === data.id ? true : false}
+              key={data.id}
+              title={data.title}
+              img={data.img}
+            ></Card>
           </div>
         );
       })}
