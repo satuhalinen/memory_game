1dc802ec0e6ed6e2c688ca5350f9ad9c37e03cb7
[satuhalinen] <satuh9@gmail.com>
2024-02-20 19:19:23 +0200

    modify card styling and add background picture

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ const App = () => {
   const [cardPairsFlipped, setCardPairsFlipped] = useState(0);
   return (
     <main>
-      <h1>Memory game</h1>
-      <h2>You have flipped {cardPairsFlipped} card pairs.</h2>
+      <div className="text">
+        <h1>Memory game</h1>
+        <h2>You have flipped {cardPairsFlipped} card pairs.</h2>
+      </div>
       <CardList
         cardPairsFlipped={cardPairsFlipped}
         setCardPairsFlipped={setCardPairsFlipped}
